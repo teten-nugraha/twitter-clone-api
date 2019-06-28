73ab214eb7477f86b7e0bb47c04a09b36f8d03bd
teten777 <teten.nugraha18@gmail.com>
2019-06-28 16:49:54 +0700

    first

---
+++ src/test/java/com/twitter/clone/twitterclone/TwitterCloneApplicationTests.java
@@ -0,0 +1,16 @@
+package com.twitter.clone.twitterclone;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TwitterCloneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
