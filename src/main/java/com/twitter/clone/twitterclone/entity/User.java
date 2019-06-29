e0f7a8c7b69667706f949d8a44ae44828c887c77
teten777 <teten.nugraha18@gmail.com>
2019-06-29 19:39:00 +0700

    update pom dan user entity

---
+++ src/main/java/com/twitter/clone/twitterclone/entity/User.java
@@ -0,0 +1,77 @@
+package com.twitter.clone.twitterclone.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.util.Collection;
+import java.util.Date;
+
+@Entity
+@Data
+@ToString
+@NoArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email(message = "Username needs to be an email")
+    @NotBlank(message = "Username is required")
+    @Column(unique = true)
+    private String username;
+
+    @NotBlank(message = "fullname is required")
+    private String fullname;
+
+    @NotBlank(message = "password is required")
+    private String password;
+
+    @Transient
+    private String confirmPassword;
+    private Date create_At;
+    private Date update_At;
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
