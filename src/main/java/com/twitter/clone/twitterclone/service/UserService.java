2427137381c1d2001749ba80eab1779087aaa1c5
teten777 <teten.nugraha18@gmail.com>
2019-06-29 19:51:40 +0700

    Create user service

---
+++ src/main/java/com/twitter/clone/twitterclone/service/UserService.java
@@ -0,0 +1,29 @@
+package com.twitter.clone.twitterclone.service;
+
+import com.twitter.clone.twitterclone.entity.User;
+import com.twitter.clone.twitterclone.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public User saveUser(User newUser) {
+        try{
+            newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+            newUser.setUsername(newUser.getUsername());
+
+            return userRepository.save(newUser);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
