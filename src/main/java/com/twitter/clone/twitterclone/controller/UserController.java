771171608c296441015189375cb368daacde632d
teten777 <teten.nugraha18@gmail.com>
2019-06-29 19:57:10 +0700

    create user controller

---
+++ src/main/java/com/twitter/clone/twitterclone/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.twitter.clone.twitterclone.controller;
+
+import com.twitter.clone.twitterclone.entity.User;
+import com.twitter.clone.twitterclone.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result) {
+
+        if(result) {
+            System.out.println(result);
+        }
+
+        User newUser = userService.saveUser(user);
+
+        return new ResponseEntity<User>(newUser, HttpStatus.CREATED);
+
+    }
+
+
+}
