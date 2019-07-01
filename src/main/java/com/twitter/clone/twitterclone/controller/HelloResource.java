7cb119767dfff35cda18bd00746ff2d948e8ca92
teten777 <teten.nugraha18@gmail.com>
2019-07-01 16:43:16 +0700

    add HelloResource

---
+++ src/main/java/com/twitter/clone/twitterclone/controller/HelloResource.java
@@ -0,0 +1,16 @@
+package com.twitter.clone.twitterclone.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloResource {
+
+    @GetMapping
+    public String helloWorld() {
+        return  "Hello World !";
+    }
+
+}
