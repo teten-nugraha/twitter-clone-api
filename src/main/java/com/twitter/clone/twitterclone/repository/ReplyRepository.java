d14d9aa75ea4052b872a900fef69dfb9fbf11b00
teten777 <teten.nugraha18@gmail.com>
2019-07-01 14:08:29 +0700

    update reply findAllByTweetId

--- src/main/java/com/twitter/clone/twitterclone/repository/ReplyRepository.java
+++ src/main/java/com/twitter/clone/twitterclone/repository/ReplyRepository.java
@@ -3,5 +3,10 @@ package com.twitter.clone.twitterclone.repository;
 import com.twitter.clone.twitterclone.entity.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
+
+    List<Reply> findAllByTweetId(Long tweetId);
+
 }
