2b35191d8e2a291b7ed017c9f0c0828af1f3ef33
teten777 <teten.nugraha18@gmail.com>
2019-07-01 13:00:55 +0700

    update get tweets me

--- src/main/java/com/twitter/clone/twitterclone/repository/TweetRepository.java
+++ src/main/java/com/twitter/clone/twitterclone/repository/TweetRepository.java
@@ -1,9 +1,16 @@
 package com.twitter.clone.twitterclone.repository;
 
 import com.twitter.clone.twitterclone.entity.Tweet;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TweetRepository extends JpaRepository<Tweet, Long> {
+
+    Page<Tweet> findAllByUserId(Long id,Pageable pageable);
+
 }
--- src/main/java/com/twitter/clone/twitterclone/service/TweetService.java
+++ src/main/java/com/twitter/clone/twitterclone/service/TweetService.java
@@ -6,7 +6,11 @@ import com.twitter.clone.twitterclone.exception.ResourceNotFoundException;
 import com.twitter.clone.twitterclone.repository.TweetRepository;
 import com.twitter.clone.twitterclone.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TweetService {
@@ -24,6 +28,14 @@ public class TweetService {
         tweet.setUser(maker);
         return tweetRepository.save(tweet);
 
+
+    }
+
+    public Page<Tweet> getTweetsMe(Pageable pageable, String makername) {
+
+        User maker = userRepository.findByUsername(makername).orElseThrow(() -> new ResourceNotFoundException("User Not Found"));
+
+        return tweetRepository.findAllByUserId(maker.getId(), pageable);
 
     }
 
